18d98c958a9b3240572528cab6726286e579f7a4
Tjeerd de Haas <tjeerd.de.haas@intradata.nl>
2016-05-11 14:58:38 +0200

    Added all runbooks

---
+++ runbooks/routing_purge.ps1
@@ -0,0 +1,48 @@
+workflow routing_purge
+{
+	param
+    (
+        # Fully-qualified name of the Azure DB server 
+        [parameter(Mandatory=$true)] 
+        [string] $SqlServerName,
+
+        # Credentials for $SqlServerName stored as an Azure Automation credential asset
+        # When using in the Azure Automation UI, please enter the name of the credential asset for the "Credential" parameter
+        [parameter(Mandatory=$true)] 
+        [PSCredential] $Credential
+    )
+
+	inlinescript
+    {
+		Write-Output "Starting routing.purge"
+		
+        # Setup credentials   
+        $ServerName = $Using:SqlServerName
+        $UserId = $Using:Credential.UserName
+        $Password = ($Using:Credential).GetNetworkCredential().Password
+
+		# Create connection for each individual database
+        $DatabaseConnection = New-Object System.Data.SqlClient.SqlConnection
+        $DatabaseCommand = New-Object System.Data.SqlClient.SqlCommand
+
+		Write-Output "Connecting to database $ServerName"
+
+		# Setup connection string
+        $DatabaseConnection.ConnectionString = "Server=$ServerName; Database=iddb-carerix-routing; User ID=$UserId; Password=$Password;"
+        $DatabaseConnection.Open();
+
+		Write-Output "Executing stored procedure"
+
+        # Create command
+        $DatabaseCommand.Connection = $DatabaseConnection
+        $DatabaseCommand.CommandText = "EXEC routing.purge"
+
+        # Execute query and return single scalar result 
+        $DatabaseCommand.ExecuteScalar()
+        
+		Write-Output "Executed routing.purge"
+			
+		# Close connection
+		$DatabaseConnection.Close();
+	}		
+}
