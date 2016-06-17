c59dc37bcf9126dffeaa57b82db3570b62651f69
IntraData <tjeerd.de.haas@intradata.nl>
2016-06-17 10:56:29 +0200

    Created routing_janitor_staging.ps1 in AzureAutomation.

---
+++ runbooks/routing_janitor_staging.ps1
@@ -0,0 +1,49 @@
+workflow routing_janitor_staging
+{
+	param
+    (
+        # Fully-qualified name of the Azure DB server 
+        [parameter(Mandatory=$true)] 
+        [string] $SqlServerName,
+
+        # Credentials for $SqlServerName stored as an Azure Automation credential asset
+        # When using in the Azure Automation UI, please enter the name of the credential asset for the "Credential" parameter
+        [parameter(Mandatory=$true)] 
+        [PSCredential] $Credential
+    )
+
+	inlinescript
+    {
+		Write-Output "Starting routing.janitor"
+		
+        # Setup credentials   
+        $ServerName = $Using:SqlServerName
+        $UserId = $Using:Credential.UserName
+        $Password = ($Using:Credential).GetNetworkCredential().Password
+
+		# Create connection for each individual database
+        $DatabaseConnection = New-Object System.Data.SqlClient.SqlConnection
+        $DatabaseCommand = New-Object System.Data.SqlClient.SqlCommand
+
+		Write-Output "Connecting to database $ServerName"
+		
+		# Setup connection string
+        $DatabaseConnection.ConnectionString = "Server=$ServerName; Database=iddb-carerix-staging; User ID=$UserId; Password=$Password;"
+        $DatabaseConnection.Open();
+
+		Write-Output "Executing stored procedure"
+			
+        # Create command
+        $DatabaseCommand.Connection = $DatabaseConnection
+        $DatabaseCommand.CommandTimeout = 120
+		$DatabaseCommand.CommandText = "EXEC routing.janitor"
+
+        # Execute query and return single scalar result 
+        $DatabaseCommand.ExecuteScalar()
+        
+		Write-Output "Executed routing.janitor"
+			
+		# Close connection
+		$DatabaseConnection.Close();
+	}		
+}
